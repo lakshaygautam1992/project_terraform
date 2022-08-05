bf7da609515f1bea3a9cd7dac1d986359079d6c9
lakshaygautam1992 <88856705+lakshaygautam1992@users.noreply.github.com>
2022-08-05 20:49:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_security_group" "lakshay_sg" {
 resource "aws_instance" "lakshay_instance" {
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.server_ami.id
-  #key_name               = aws_key_pair.lakshaykey.id
+  key_name               = "terraform_key"
   vpc_security_group_ids = [aws_security_group.lakshay_sg.id]
   subnet_id              = aws_subnet.lakshay_public_subnet.id
   user_data              = file("userdata.tpl")
@@ -89,7 +89,7 @@ resource "aws_instance" "lakshay_instance" {
   }
 
   tags = {
-    Name = "terraform-server-2"
+    Name = "terraform-server-3"
   }
   
  # provisioner "local-exec" {
