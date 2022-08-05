8d600413abca498a8dc3e90a80b32008e9d7950a
lakshaygautam1992 <88856705+lakshaygautam1992@users.noreply.github.com>
2022-08-05 22:07:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "host_os" {
     type = string
 }
+variable "test" {
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfjtQT+a8Yy5ly5yUIsRsJ9kCT7cIhPOoKuQKFxXIncqkUTruFyO++O77zIp1SIMXzyKb4shkpFei3ShP9pbbEfy+uTLjHYcpPLJNfjA/4zh7MiHEqNfQf/dXitBcfu4P5qI1CBJTEL4fUgj6CIBGu1bZSCtzCq1UY/0S7Zv0YIdFAoFBHMdJcSmfLOiqWeopu0Q/V10+0WJNY03iFRIqUHrlxNqUJxvUALo2j1okNwIVqcprizybh96TBbmcryO2S3GnJV66RUwyK8UkkfRxWi/9M6btn1l8kVfk6v4FxeN8iToCQrmo2HswiM6aW8+H99gWNHOJgRdxlBKOyE7Ul root@ip-10-252-1-47.ec2.internal"
+    sensitive = true
+}
