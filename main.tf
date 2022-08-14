bb4d4536c7f605d6a608d8a7e97a4ce53d3f8a1e
lakshaygautam1992 <88856705+lakshaygautam1992@users.noreply.github.com>
2022-08-14 19:20:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,8 +74,8 @@ resource "aws_security_group" "lakshay_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-#resource "aws_key_pair" "lakshay_key" {
-#  key_name = "rakesh_key"
+resource "aws_key_pair" "lakshay_key" {
+  key_name = "rakesh_key"
 #  public_key = var.test
 #  public_key = "{file("test_key.pub")}"
  
@@ -85,7 +85,7 @@ resource "aws_security_group" "lakshay_sg" {
 resource "aws_instance" "lakshay_instance" {
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.server_ami.id
-#  key_name               = "rakesh_key"
+  aws_key_pair               = aws_key_pair.lakshay_key.id
   vpc_security_group_ids = [aws_security_group.lakshay_sg.id]
   subnet_id              = aws_subnet.lakshay_public_subnet.id
   user_data              = file("userdata.tpl")
